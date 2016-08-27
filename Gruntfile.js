ee800950d1967ad034388be53516bed8605f1c59
Gilberto Tavares <camaleaun@outlook.com>
2016-08-27 07:05:02 -0300

    Ace Build

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,35 @@ module.exports = function (grunt) {
     shell: {
       bowerInstall: {
         command: 'bower install'
+      },
+      cloneAce: {
+        command: 'git clone https://github.com/ajaxorg/ace.git editor/ace/.ace.tmp && mv editor/ace/.ace.tmp/* editor/ace/ && rm editor/ace/.ace.tmp/.gitignore && mv editor/ace/.ace.tmp/.* editor/ace/ && rm -d editor/ace/.ace.tmp/'
+      },
+      clearAce: {
+        command: 'rm -r editor/ace/lib/ace/ext/* editor/ace/lib/ace/snippets/* editor/ace/lib/ace/theme/'
+      },
+      startAceNpm: {
+        command: 'npm install --prefix editor/ace'
+      },
+      copyUalAce: {
+        command: 'cp -r editor/ace/mode/ editor/ace/lib/ace/ && cp -r editor/ace/theme/ editor/ace/lib/ace/'
+      },
+      clearAceMode: {
+        command: "find editor/ace/lib/ace/mode/. ! -name 'text*' -exec rm -r -f {} + || rm editor/ace/lib/ace/mode/texti*"
+      },
+      buildAce: {
+        command: 'node editor/ace/Makefile.dryice.js -m -nc --target editor/ace/build'
+      },
+      removeAce: {
+        command: 'rm -r editor/ace/a* editor/ace/b* editor/ace/d* editor/ace/e* editor/ace/l* editor/ace/to*'
+      }
+    },
+
+    concat: {
+      aceUal: {
+        files: {
+          'editor/js/aceual.js': ['editor/ace/build/src-min-noconflict/ace.js', 'editor/ace/build/src-min-noconflict/mode-ual.js', 'editor/ace/build/src-min-noconflict/theme-ual.js']
+        },
       }
     },
 
@@ -110,6 +139,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('style', ['sass', 'autoprefixer']);
 
