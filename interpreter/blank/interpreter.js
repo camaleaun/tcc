5d61a00aee199fd1435df6cbd2042f6cb462e768
Gilberto Tavares <camaleaun@outlook.com>
2016-09-04 16:56:44 -0300

    Blank sctruct based in math alternative

---
+++ interpreter/blank/interpreter.js
@@ -0,0 +1,10 @@
+/*jslint devel: true*/
+var JSrun = function () {
+        'use strict';
+    },
+    init = function () {
+        'use strict';
+        document.getElementById('run').addEventListener('click', JSrun);
+    };
+
+document.addEventListener('DOMContentLoaded', init);
