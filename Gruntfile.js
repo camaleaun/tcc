5371848ceccbecca60c8a88a82038402ab653485
Gilberto Tavares <camaleaun@outlook.com>
2016-11-03 15:29:55 -0200

    Current progress of paper

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/*jslint node: true*/
+
+module.exports = function (grunt) {
+
+  'use strict';
+
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
+  // Project configuration.
+  grunt.initConfig({
+    // Meta data
+    pkg: grunt.file.readJSON('package.json'),
+
+    shell: {
+      pdflatex: {
+        command: 'pdflatex papper && bibtex papper && pdflatex papper && pdflatex papper'
+      }
+    },
+
+    watch: {
+      monograph: {
+        files: [
+          '*.tex',
+          '*.cls',
+          '*.bib'
+        ],
+        tasks: ['pdflatex']
+      }
+    }
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('pdflatex', ['shell:pdflatex']);
+
+  grunt.registerTask('default', ['watch']);
+
+};
